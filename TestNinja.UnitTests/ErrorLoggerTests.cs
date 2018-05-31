93fe96d5034d95c358f956f77772ba948c80ccf0
Scott Edwards <scotteza@gmail.com>
2018-05-31 11:01:49 +0200

    Added basic ErrorLoggerTests.

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_ShouldSetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("     ")]
+        public void Log_InvalidErrorText_ShouldThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+            // Equivalent to:
+            //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>);
+        }
+    }
+}
