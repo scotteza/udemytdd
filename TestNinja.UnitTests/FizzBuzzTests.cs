ebc364e4f7b66aa080d79faad8400d2b683abb57
Scott Edwards <scotteza@gmail.com>
2018-05-31 16:55:14 +0200

    Added FizzBuzzTests.

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        private FizzBuzz _fizzBuzz;
+
+        [Test]
+        public void GetOutput_WhenCalledWithMultipleOf5And3_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenCalledWithMultipleOf5AndNot3_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenCalledWithMultipleOf3AndNot5_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_WhenCalledWithNumberThatIsNotAMultipleOf3AndNotAMultipleOf5_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(7);
+
+            Assert.That(result, Is.EqualTo("7"));
+        }
+    }
+}
