d9f524a9969c5af1e5704831c50b6fac0b44c6da
Scott Edwards <scotteza@gmail.com>
2018-06-01 20:23:33 +0200

    Changed video service to use constructor injection.

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -8,20 +8,18 @@ namespace TestNinja.Mocking
 {
     public class VideoService
     {
-        public IFileReader FileReader { get; set; }
+        private readonly IFileReader _fileReader;
 
-        public VideoService()
+        public VideoService(IFileReader fileReader = null)
         {
-            FileReader = new FileReader();
+            _fileReader = fileReader ?? new FileReader();
         }
 
         public string ReadVideoTitle()
         {
-            var str = FileReader.Read("video.txt");
+            var str = _fileReader.Read("video.txt");
             var video = JsonConvert.DeserializeObject<Video>(str);
-            if (video == null)
-                return "Error parsing the video.";
-            return video.Title;
+            return video == null ? "Error parsing the video." : video.Title;
         }
 
         public string GetUnprocessedVideosAsCsv()
